e440476c9bff5e3bd70201f5d1049ffdbc103497
Yoyo Peng <yoyopyeah@gmail.com>
2022-08-16 01:46:25 -0400

    set up, profile side

---
+++ src/Data/FollowersData.js
@@ -0,0 +1,27 @@
+import img1 from "../img/img1.png";
+import img2 from "../img/img2.png";
+import img3 from "../img/img3.png";
+import img4 from "../img/img4.jpg";
+
+export const Followers = [
+  { 
+    name: "User1",
+    username: "user1", 
+    img: img1
+  },
+  { 
+    name: "User2",
+    username: "user2", 
+    img: img2
+  },
+  { 
+    name: "User3",
+    username: "user3", 
+    img: img3
+  },
+  { 
+    name: "User4",
+    username: "user4", 
+    img: img4
+  }
+];
