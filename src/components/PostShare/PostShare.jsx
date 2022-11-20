c3725213d53d12d0a313336034d9b812c3a1e135
Yoyo Peng <yoyopyeah@gmail.com>
2022-11-20 16:23:59 -0500

    PostSide

---
+++ src/components/PostShare/PostShare.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostShare = () => {
+  return (
+    <div>PostShare</div>
+  )
+}
+
+export default PostShare
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ProfileSide from '../../components/profileSide/ProfileSide'
+import PostSide from '../../components/PostSide/PostSide'
 import './Home.css'
 
 const Home = () => {
   return (
     <div className="Home">
       <ProfileSide/>
-      <div className="postSide">Posts</div>
+      <PostSide/>
       <div className="RightSide">Rightside</div>
     </div>
   )
